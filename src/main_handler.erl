8ba8a4c1155cd4e4d7f72528826d8662b25b46df
unknown <mypcdk1@gmail.com>
2024-11-08 21:04:43 +0100

    fix 2

--- src/main_handler.erl
+++ src/main_handler.erl
@@ -5,15 +5,15 @@
 
 init(Req, State) ->
 	Req2 = cowboy_req:stream_reply(200, #{<<"content-type">> => <<"text/plain">>}, Req),
-	recurse("hi.html", Req2, State),
+	recurse({["C:/Users/mustafa/Desktop/cowboy_load_html_partially/src"],"hi.html"}, Req2, State),
 	{cowboy_loop, Req2, State}.
 
 info(eof, Req, State) ->
 	cowboy_req:stream_body("", fin, Req),
     {stop, Req, State};
 info({event, Data}, Req, State) ->
-	case file:read_line(Data) of
-    {ok, C} -> cowboy_req:stream_body(<<C>>, nofin, Req),
+	case file:read(Data, 100) of
+    {ok, C} -> cowboy_req:stream_body(C, nofin, Req),
 	info({event, Data}, Req, State);
 	_ -> info(eof, Req, State), 
 		file:close(Data)
@@ -23,7 +23,7 @@ info(_Msg, Req, State) ->
     {ok, Req, State}.
 
 recurse(A, Req, State) ->
-	case file:open(A, [read]) of
-		{ok, B} -> info({event, B}, Req, State);
+	case file:path_open(element(1, A), element(2, A), [read]) of
+		{ok, B, _C} -> info({event, B}, Req, State);
 		_ -> "nothing"
 	end.
